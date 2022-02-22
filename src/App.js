a18e9c77e38e9fd0f73c371aa3c448574bd9ccc7
yuki4san <yuki.shimura@webempath.com>
2022-02-22 10:46:10 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './styles/App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+import React from "react";
+
+// Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
+const TWITTER_HANDLE = 'あなたのTwitterのハンドルネームを貼り付けてください';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const OPENSEA_LINK = '';
+const TOTAL_MINT_COUNT = 50;
+
+const App = () => {
+  // renderNotConnectedContainer メソッドを定義します。
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">
+            あなただけの特別な NFT を Mint しよう💫
+          </p>
+          {renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
