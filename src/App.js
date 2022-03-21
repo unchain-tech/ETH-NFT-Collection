ca92e2c8c50e253ac929f7f12a102cc99c83a7c8
Alejandro Cremades <ale@alecrem.com>
2022-03-21 23:39:26 +0900

    Step 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
+// App.js
+import React, { useEffect } from "react";
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
-
 // Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
 const TWITTER_HANDLE = 'あなたのTwitterのハンドルネームを貼り付けてください';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
-
 const App = () => {
+  const checkIfWalletIsConnected = () => {
+    /*
+    * ユーザーがMetaMaskを持っているか確認します。
+    */
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  }
   // renderNotConnectedContainer メソッドを定義します。
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
-
+  /*
+  * ページがロードされたときに useEffect()内の関数が呼び出されます。
+  */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
   return (
     <div className="App">
       <div className="container">
@@ -24,6 +40,7 @@ const App = () => {
           <p className="sub-text">
             あなただけの特別な NFT を Mint しよう💫
           </p>
+          {/* メソッドを追加します */}
           {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
@@ -39,5 +56,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
