7119613548ca25c84a55a643d1c0a0d1d20ee4b0
kitashige <kitashige.ukiyoe@gmail.com>
2022-07-25 05:27:55 +0900

    定数定義を別ファイルに移動

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import twitterLogo from './assets/twitter-logo.svg';
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
-// Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
-const TWITTER_HANDLE = 'kitashige_web3';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
-const MINT_PRICE = "0.001";
-
-//const CONTRACT_ADDRESS = "0xf1ef7780590252ad68790CC1A1dC3C832461F399";
-const CONTRACT_ADDRESS = "0x739c5Ff5A70AE5aB44e4941f3FA97Ca659D83581";
+import {
+  CONTRACT_ADDRESS,
+  TWITTER_HANDLE,
+  TWITTER_LINK,
+  TOTAL_MINT_COUNT,
+  MINT_PRICE,
+} from "./constants";
 
 const App = () => {
   /*
